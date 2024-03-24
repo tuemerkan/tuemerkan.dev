46acc97b86ad74099c6a6ec5fed5e42e99b43054
tuemerkan <tuemerkan.d@icloud.com>
2024-03-24 07:05:36 +0100

    add projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,63 @@
 'use client'
 
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent
+} from '@/components/ui/card'
+import Image from 'next/image'
+
 export default function Projects() {
-  return <></>
+  return (
+    <>
+      <a href="https://getrespondo.com" target="_blank">
+        <Card className="flex">
+          <Image
+            src="/images/respondo.svg"
+            width={50}
+            height={50}
+            alt="respondo"
+            className="pl-5"
+          />
+          <div>
+            <CardHeader>
+              <CardTitle>Respondo</CardTitle>
+              <CardDescription>
+                Generate engaging twitter replies with AI.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Elevate your Twitter game with real-time, AI-generated reply
+                suggestions directly within your Chrome browser.
+              </p>
+            </CardContent>
+          </div>
+        </Card>
+      </a>
+
+      <Card className="flex">
+        <Image
+          src="/images/progress.svg"
+          width={50}
+          height={50}
+          alt="progress"
+          className="pl-5"
+        />
+        <div>
+          <CardHeader>
+            <CardTitle>Project Name: AirVillas </CardTitle>
+            <CardDescription>In progress</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>
+              Booking platform especially for accommodation with full privacy.
+            </p>
+          </CardContent>
+        </div>
+      </Card>
+    </>
+  )
 }
